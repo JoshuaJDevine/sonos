4f0fadbef3e5d30d57098948ec62bf212ea9ef0c
JoshuaJDevine <jd8386@gmail.com>
2021-04-22 09:34:32 -0700

    Basic Auth Flow

---
+++ server/db/seeders/20210422160910-demo-user.js
@@ -0,0 +1,67 @@
+'use strict';
+const faker = require('faker');
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'LudwigB@ArchdukeRudplh.estate',
+        username: 'LudwigB',
+        hashedPassword: bcrypt.hashSync('DONALDFRANCISTOVEY'),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'iDente',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'tionRO',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'ophaph',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'beryac',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'riable',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'aTeNTi',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'SiLenT',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'MaNTre',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+      {
+        email: faker.internet.email(),
+        username: 'loner',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -4,8 +4,9 @@
 \\=====================//
  */
 const router = require('express').Router();
-
-
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 
 /*
@@ -13,9 +14,39 @@ const router = require('express').Router();
 ||TEST ROUTES----------||
 \\=====================//
  */
+//Test POST route
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
 });
+
+//Test Auth flow - token
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'LudwigB'
+//         },
+//     })
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+// }));
+
+//Test Auth flow - restore user
+// router.get(
+//     '/restore-user',
+//     restoreUser,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+//Test Auth flow - require auth
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
 
 
 
